b933c195e6defd55ec1ffc60900f82a88997d52a
Alex429429 <sasha.ignatenko07@gmail.com>
2025-06-04 16:28:40 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import time
+import os
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    server_time = int(time.time() * 1000)
+    return jsonify({'serverTime': server_time})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
