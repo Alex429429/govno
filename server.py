cd4a646af489769ed83d9e60dba99e8db7171258
Alex429429 <sasha.ignatenko07@gmail.com>
2025-06-04 16:51:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, jsonify
 import time
 import os
-from datetime import datetime
 
 app = Flask(__name__)
-
-def format_time(ms):
-    return datetime.fromtimestamp(ms / 1000).strftime('%H:%M:%S.%f')[:-3]
 
 @app.route('/ping', methods=['POST'])
 def ping():
@@ -16,19 +12,11 @@ def ping():
     packet_id = data.get('packet_id')
     master_send_time = data.get('master_send_time')
 
-    slave_send_time = int(time.time() * 1000)
-
-    # 🧾 Отладка в консоли Render
-    print(f"\n📦 Пакет #{packet_id}")
-    print(f"  Время отправки мастером:     {format_time(master_send_time)}")
-    print(f"  Время приёма на слейве:      {format_time(slave_receive_time)}")
-    print(f"  Время отправки с слейва:     {format_time(slave_send_time)}")
-
+    # Возвращаем в ответе время приема пакета на сервере, без использования локального времени отправки
     return jsonify({
         'packet_id': packet_id,
         'master_send_time': master_send_time,
-        'slave_receive_time': slave_receive_time,
-        'slave_send_time': slave_send_time
+        'slave_receive_time': slave_receive_time
     })
 
 if __name__ == '__main__':
